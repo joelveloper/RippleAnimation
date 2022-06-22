175abd0c78034b2d051fab2872aeddf16886ea6d
joelveloper <alfonzojoel@hotmail.com>
2022-06-22 18:19:46 -0400

    first commit

---
+++ RippleAnimation/RippleAnimation/Views/CircleAnimationPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace RippleAnimation.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CircleAnimationPage : ContentPage
+    {
+        public CircleAnimationPage()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnBack_Clicked(object sender, EventArgs e)
+        {
+            _ = MainThread.InvokeOnMainThreadAsync(async () => await ExecuteRectangleAnimationAndNavigateBack());
+        }
+
+        private async Task ExecuteRectangleAnimationAndNavigateBack()
+        {
+            new Animation
+            {
+                { 0, 0.01, new Animation(v => RectangleIn.IsVisible = true) },
+                { 0.01, 0.5, new Animation(v => RectangleIn.ScaleTo(15)) },
+                { 0.5, 0.6, new Animation(v => BtnBack.FadeTo(0, 100)) },
+                { 0.5, 0.51, new Animation(v => RectangleIn.FadeTo(0, 100)) }
+            }.Commit(this, "rectangleAnimation", 60, 1500, Easing.Linear);
+
+            await Task.Delay(1500);
+
+            Navigation.InsertPageBefore(new MainPage(), this);
+            _ = await Navigation.PopAsync(false);
+        }
+    }
+}
---
+++ RippleAnimation/RippleAnimation/Views/MainPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace RippleAnimation.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void ImageButton_Clicked(object sender, EventArgs e)
+        {
+            _ = MainThread.InvokeOnMainThreadAsync(async () => await ExecuteCirclesAnimationAndNavigate());
+        }
+
+        private async Task ExecuteCirclesAnimationAndNavigate()
+        {
+            new Animation
+            {
+                { 0, 0.01, new Animation(v => CircleIn.IsVisible = true) },
+                { 0.01, 0.5, new Animation(v => CircleIn.ScaleTo(15)) },
+                { 0.5, 0.51, new Animation(v => CircleIn.IsVisible = false) },
+                { 0.5, 0.51, new Animation(v => CircleOut.IsVisible = true) },
+                { 0.51, 0.6, new Animation(v => ToolBar.FadeTo(0, 150)) },
+                { 0.51, 0.6, new Animation(v => content.FadeTo(0, 150)) },
+                { 0.51, 0.6, new Animation(v => frame.FadeTo(0, 150)) },
+                { 0.51, 1, new Animation(v => CircleOut.ScaleTo(0)) },
+                { 0.99, 1,  new Animation(v => CircleOut.IsVisible = false) }
+            }.Commit(this, "circleAnimation", 60, 1500, Easing.Linear);
+
+            await Task.Delay(1500);
+
+            await Navigation.PushAsync(new CircleAnimationPage(), false);
+        }
+    }
+}
